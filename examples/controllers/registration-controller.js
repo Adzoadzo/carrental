3faa5ba2c66c549665ba5cf62755dc5cfc8c552a
mujoh <m.hadzimehanovic@gmail.com>
2018-05-04 17:30:13 +0200

    registration

--- examples/controllers/registration-controller.js
+++ examples/controllers/registration-controller.js
@@ -1,3 +1,10 @@
 function RegistrationController($scope, $http){
     console.log("Hello from Registration Controller");
+
+    $scope.add_user = function(){
+        $http.post('/user', $scope.user).then(function(data){
+          $scope.user = null;
+          $scope.user_list.push(data);
+        });
+      }
 }
--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 
+var mongo = require('mongojs');
+var db = mongo('localhost:27017/carrental', ['users'])
+
+var body_parser = require('body-parser');
+app.use(body_parser.json());
+
 app.use('/', express.static('examples'));
+
+app.get('/users', function(req, res){
+    db.users.find(function(err, docs){
+     res.json(docs);
+   });
+  });
+
+app.post('/user', function(req, res){
+    req.body._id = null;
+    db.users.insert(req.body, function(err, doc){
+     res.json(doc);
+    });
+ });
 
 app.listen(3000, () => console.log('Car rental app is listening on port 3000!'));
