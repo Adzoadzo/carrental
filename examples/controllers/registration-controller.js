e0a157e9f1bdc3e3c2793081413499a21829516b
mujoh <m.hadzimehanovic@gmail.com>
2018-05-18 12:01:46 +0200

    single car added

--- examples/controllers/registration-controller.js
+++ examples/controllers/registration-controller.js
@@ -1,9 +1,10 @@
-function RegistrationController($scope, $http){
+function RegistrationController($scope, $http, toastr){
     console.log("Hello from Registration Controller");
 
     $scope.add_user = function(){
         $http.post('/register', $scope.user).then(function(data){
           $scope.user = null;
+          toastr.success("You are successfully registered!", "Registration Successfull!")
           //$scope.user_list.push(data);
         });
       }
