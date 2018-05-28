d3ead596c03ee92e79b75bb7b098dee20baddd7c
mujoh <m.hadzimehanovic@gmail.com>
2018-05-28 16:50:41 +0200

    Finished orders design

--- examples/controllers/orders-controller.js
+++ examples/controllers/orders-controller.js
@@ -6,8 +6,10 @@ function OrdersController($scope, $http, $routeParams){
         $scope.car_name = $routeParams.car_name;
         $scope.car_price = $routeParams.car_price;
         $scope.pickup_location = $routeParams.pickup_location;
+        $scope.pickup_date = $routeParams.pickup_date;
         $scope.pickup_time = $routeParams.pickup_time;
         $scope.return_location = $routeParams.return_location;
+        $scope.return_date = $routeParams.return_time;
         $scope.return_time = $routeParams.return_time;
     }
 }
